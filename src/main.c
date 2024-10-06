5e5e8778aa1a8fd84cad40735597a2f3992228f0
Dmitry Volkov <lorddmitry22@gmail.com>
2024-10-07 02:07:05 +0300

    Add handle_sigint

--- src/main.c
+++ src/main.c
@@ -1,15 +1,45 @@
 #include "monkeybot.h"
 #include <signal.h>
 #include <unistd.h>
+#include <pthread.h>
 
+Telebot bot;
+int isInitialized = 0;
+pthread_t init_thread;
+
+void *init_bot(void *arg) {
+    if (telebot_init(&bot, "resources/token.txt") == -1) {
+        fprintf(stderr, "Failed to initialize bot.\n");
+        return NULL;
+    }
+    isInitialized = 1;
+    printf("Bot initialized in thread.\n");
+    return NULL;
+}
+
+void handle_sigint(int sig) {
+    printf("\nCompletion of the program...\n");
+    while (isInitialized == 0) {
+        // Backoff
+        sleep(1);
+    }
+    telebot_destroy(&bot);
+    printf("Bye!\n\n");
+    exit(0);
+}
 
 int main(){
-    Telebot bot;
+    signal(SIGINT, handle_sigint);
 
-    if (telebot_init(&bot, "resources/token.txt") == -1) {
+    if (pthread_create(&init_thread, NULL, init_bot, NULL) != 0) {
+        fprintf(stderr, "Failed to create init thread.\n");
         return -1;
     }
-    
+    pthread_join(init_thread, NULL);
+    if (!isInitialized) {
+        return -1;
+    }
+
     printf("Bot started\n\n");
     while (1) {
         char response[RESPONSE_SIZE];
